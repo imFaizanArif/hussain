55aa4ac677a23b9db91ca7d9a50232accc6d4b5c
imFaizanArif <faizanarif1245@gmail.com>
2025-01-14 20:59:30 +0500

    more components added

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -18,7 +18,7 @@ export const Project: React.FC<ProjectProps> = ({ bgColor, flow, cardFlow, img, 
                     <LeftCard flow={cardFlow} />
                 </div>
                 {img2 && <div className={`flex flex-col md:flex-row ${flow} pt-0 pb-[60px] md:pt-[30px] px-[30px] sm:px-[30px] md:[px-45] lg:px-[60px] mx-auto`}>
-                    <img src={img2} alt="" className={`${imgSize} mr-0 md:mr-24 mt-12 sm:mt-12 md:mt-0`} />
+                    <img src={img2} alt="" className={`${imgSize} mr-0 md:mr-24`} />
                 </div>}
             </div>
         </div>
