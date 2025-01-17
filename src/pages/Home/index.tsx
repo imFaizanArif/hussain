26aa65d2a80c5d1c79cfd0621587bc8129852407
imFaizanArif <faizanarif1245@gmail.com>
2025-01-18 00:54:31 +0500

    updated components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { DesignProcess, Hero, PartTwo, Problem, Project, ProjectOverview } from "@/components"
+import { DesignProcess, Hero, Insights, PartThree, PartTwo, Problem, Project, ProjectOverview } from "@/components"
 
 export const Home = () => {
     return (
@@ -13,7 +13,7 @@ export const Home = () => {
             <PartTwo bgColor="bg-grid-purple-200" flow="md:flex-row flex-col-reverse" cardFlow="flex flex-col items-end text-right" img="/assets/project3.png" />
             <DesignProcess img1="/assets/project4.png" img2="/assets/project5.png" />
             <DesignProcess bgColor="bg-grid-purple-50" img2="/assets/project6.png" flow="flex-col justify-start items-start" />
-            {/* <Project img="/assets/project7.png" flow="flex-col-reverse justify-between items-center" cardFlow="flex flex-col items-end text-right" imgSize="w-full md:w-1/2" />
+            <Project img="/assets/project7.png" flow="flex-col-reverse justify-between items-center" cardFlow="flex flex-col items-end text-right" imgSize="w-full md:w-1/2" />
             <PartThree bgColor="bg-grid-purple-600" color="text-white" img="/assets/project8.png" />
             <Project img="/assets/project14.png" flow="flex-col-reverse justify-between items-center" imgSize="w-full md:w-1/2" />
             <DesignProcess img1="/assets/project9.png" bgColor="bg-grid-purple-200" imgSize="w-full md:w-[80%] ml-0 md:ml-24 mb-12 sm:mb-12 md:mb-0" />
@@ -26,7 +26,7 @@ export const Home = () => {
             <DesignProcess img1="/assets/project16.png" />
             <Project bgColor="bg-grid-purple-200" img="/assets/project17.png" flow="flex-col-reverse justify-between items-center" img2="/assets/project18.png" />
             <DesignProcess img1="/assets/project14.png" />
-            <Project bgColor="bg-grid-purple-50" img="/assets/project17.png" flow="flex-col-reverse justify-between items-center" cardFlow="flex flex-col items-end text-right" img2="/assets/project19.png" /> */}
+            <Project bgColor="bg-grid-purple-50" img="/assets/project17.png" flow="flex-col-reverse justify-between items-center" cardFlow="flex flex-col items-end text-right" img2="/assets/project19.png" />
         </div>
     )
 }
